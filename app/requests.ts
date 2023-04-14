40657559b8f77438b7fee8e0d5e024d05acd491a
Luke Li <1010438846@qq.com>
2023-04-14 09:07:19 +0800

    Update requests.ts
    rollback2

--- app/requests.ts
+++ app/requests.ts
@@ -145,7 +145,7 @@ export async function requestChatStream(
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        path: "v1/completions",
+        path: "v1/chat/completions",
         ...getHeaders(),
       },
       body: JSON.stringify(req),
